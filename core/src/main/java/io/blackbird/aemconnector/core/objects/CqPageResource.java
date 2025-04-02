ab6142e00a4801eede4aa00ce560dc1810bbcbbe
vadymkaravayev <vadym_karavayev_tmg@whirlpool.com>
2025-04-02 19:25:45 +0300

    Create Page Exporter Servlet

---
+++ core/src/main/java/io/blackbird/aemconnector/core/objects/CqPageResource.java
@@ -0,0 +1,34 @@
+package io.blackbird.aemconnector.core.objects;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceWrapper;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+public class CqPageResource extends ResourceWrapper {
+
+    /**
+     * Creates a new wrapper instance delegating all method calls to the given
+     * <code>resource</code>.
+     *
+     * @param resource The resource to wrap
+     */
+    public CqPageResource(Resource resource) {
+        super(resource);
+    }
+
+    @Override
+    public Iterator<Resource> listChildren() {
+        return Optional.ofNullable(getChild("jcr:content"))
+                .map(res -> List.of(res).iterator())
+                .orElse(Collections.emptyIterator());
+    }
+
+    @Override
+    public Iterable<Resource> getChildren() {
+        return this::listChildren;
+    }
+}
---
+++ core/src/main/java/io/blackbird/aemconnector/core/servlets/BlackbirdPageExportServlet.java
@@ -0,0 +1,78 @@
+package io.blackbird.aemconnector.core.servlets;
+
+import com.day.cq.wcm.api.Page;
+import io.blackbird.aemconnector.core.exceptions.BlackbirdHttpErrorException;
+import io.blackbird.aemconnector.core.objects.CqPageResource;
+import io.blackbird.aemconnector.core.servlets.internal.BlackbirdAbstractBaseServlet;
+import io.blackbird.aemconnector.core.utils.ResourceJsonUtil;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.SlingHttpServletResponse;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.api.servlets.HttpConstants;
+import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
+import org.osgi.service.component.annotations.Component;
+
+import javax.servlet.Servlet;
+import javax.servlet.http.HttpServletResponse;
+import java.io.Serializable;
+
+@Component(service = Servlet.class)
+@SlingServletResourceTypes(
+        resourceTypes = BlackbirdPageExportServlet.RESOURCE_TYPE,
+        methods = HttpConstants.METHOD_GET
+)
+public class BlackbirdPageExportServlet extends BlackbirdAbstractBaseServlet {
+    public static final String RESOURCE_TYPE = "bb-aem-connector/services/page-exporter";
+    public static final String PAGE_PATH_PARAM = "pagePath";
+
+    @Override
+    public Serializable getSerializableObject(SlingHttpServletRequest request, SlingHttpServletResponse response) throws BlackbirdHttpErrorException {
+        String pagePath = request.getParameter(PAGE_PATH_PARAM);
+
+        Resource resource = getResource(request, pagePath);
+
+        Page page = resource.adaptTo(Page.class);
+
+        if (page == null) {
+            throw new BlackbirdHttpErrorException(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "Bad Request", String.format("'%s' is not a cq Page", pagePath));
+        }
+
+        Resource jcrContent = page.getContentResource();
+
+        if (jcrContent == null) {
+            throw new BlackbirdHttpErrorException(
+                    HttpServletResponse.SC_CONFLICT,
+                    "Conflict", String.format("Page exists but has no jcr:content node. Path: %s", pagePath));
+        }
+
+        return ResourceJsonUtil.serializeRecursively(new CqPageResource(resource));
+    }
+
+    private static Resource getResource(SlingHttpServletRequest request, String pagePath) throws BlackbirdHttpErrorException {
+        if (pagePath == null) {
+            throw new BlackbirdHttpErrorException(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "Bad Request", String.format("Missing required query parameter: '%s'", PAGE_PATH_PARAM));
+        }
+
+        if (!pagePath.startsWith("/")) {
+            throw new BlackbirdHttpErrorException(
+                    HttpServletResponse.SC_BAD_REQUEST,
+                    "Bad Request", "pagePath must be an absolute JCR path starting with '/'"
+            );
+        }
+
+        ResourceResolver resourceResolver = request.getResourceResolver();
+        Resource resource = resourceResolver.getResource(pagePath);
+
+        if (resource == null) {
+            throw new BlackbirdHttpErrorException(
+                    HttpServletResponse.SC_NOT_FOUND,
+                    "Not Found", String.format("'%s' was not found", pagePath));
+        }
+        return resource;
+    }
+}
