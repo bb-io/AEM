b7d0b87a9e5fadb87a63b4045bc89c7170e13d59
Vadym Karavayev <vadym.karavayev@devhandler.com>
2025-06-10 15:46:46 +0300

    Fix sonar issues
    (cherry picked from commit da6c85966294b3160881e14253576f733e54ef66)

--- core/src/main/java/io/blackbird/aemconnector/core/dto/v2/ContentReference.java
+++ core/src/main/java/io/blackbird/aemconnector/core/dto/v2/ContentReference.java
@@ -1,5 +1,6 @@
 package io.blackbird.aemconnector.core.dto.v2;
 
+import com.day.cq.wcm.api.constants.NameConstants;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -15,12 +16,13 @@ public class ContentReference implements Serializable {
     String referencePath;
 
     @JsonCreator
-    public ContentReference(@JsonProperty("propertyName") String propertyName,
+    public ContentReference(@JsonProperty(NameConstants.PN_DT_NAME) String propertyName,
                             @JsonProperty("propertyPath") String propertyPath,
                             @JsonProperty("referencePath") String referencePath) {
         this.propertyName = propertyName;
         this.propertyPath = propertyPath;
         this.referencePath = referencePath;
+
     }
 
     public ContentReference(String referencePath) {
