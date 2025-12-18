063f7a99090ef77f8555c2aefe64d3753b366bb8
Alisa Bilous <alisa.bilous@devhandler.com>
2025-12-18 14:00:43 +0200

    Updated imports

--- core/src/main/java/io/blackbird/aemconnector/core/utils/Node2JsonUtil.java
+++ core/src/main/java/io/blackbird/aemconnector/core/utils/Node2JsonUtil.java
@@ -16,7 +16,6 @@ import javax.jcr.PropertyType;
 import javax.jcr.RepositoryException;
 import javax.jcr.Value;
 
-import static com.day.cq.wcm.api.constants.NameConstants.NT_PAGE;
 import static io.blackbird.aemconnector.core.utils.PageInheritanceUtils.shouldExportProperty;
 import static com.day.cq.wcm.api.NameConstants.NT_PAGE;
 import static org.apache.jackrabbit.JcrConstants.JCR_PRIMARYTYPE;
